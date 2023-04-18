6aa96747e56e99bdd6406acbd58b1a0ac8681d3e
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-04-18 11:54:47 +0900

    🔥 - Remove Adaptor for `client_scope`.
     - Client scope is a separate table but should be included in the client.

--- application/src/adaptor/client.rs
+++ application/src/adaptor/client.rs
@@ -6,26 +6,11 @@ pub trait RegisterClientAdaptor: 'static + Sync + Send {
 }
 
 #[async_trait::async_trait]
-pub trait RegisterClientScopeAdaptor: 'static + Sync + Send {
-    async fn register(&self) -> Result<(), ApplicationError>;
-}
-
-#[async_trait::async_trait]
 pub trait UnRegisterClientAdaptor: 'static + Sync + Send {
     async fn unregister(&self) -> Result<(), ApplicationError>;
 }
 
 #[async_trait::async_trait]
-pub trait UnRegisterClientScopeAdaptor: 'static + Sync + Send {
-    async fn unregister(&self) -> Result<(), ApplicationError>;
-}
-
-#[async_trait::async_trait]
 pub trait UpdateClientAdaptor: 'static + Sync + Send {
-    async fn update(&self) -> Result<(), ApplicationError>;
-}
-
-#[async_trait::async_trait]
-pub trait UpdateClientScopeAdaptor: 'static + Sync + Send {
     async fn update(&self) -> Result<(), ApplicationError>;
 }
