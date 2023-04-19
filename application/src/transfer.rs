54411131826a181a72c9f6902d193b1d8f67dbdf
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-04-19 15:34:27 +0900

    🔨 - Implemented `ClientDto`.

--- application/src/transfer.rs
+++ application/src/transfer.rs
@@ -1,2 +1,3 @@
 pub mod account;
 pub mod token;
+pub mod client;
---
+++ application/src/transfer/client.rs
@@ -0,0 +1,58 @@
+use kernel::entities::{Client, DestructClient, ScopedObject, DestructScopedObject};
+use uuid::Uuid;
+
+#[derive(Debug)]
+pub struct ClientDto {
+    pub id: Uuid,
+    pub name: String,
+    pub desc: String,
+    pub uris: Option<Vec<String>>,
+    pub types: Option<String>,
+    pub scopes: Vec<ScopeDto>
+}
+
+#[derive(Debug)]
+pub struct ScopeDto {
+    pub method: String,
+    pub description: String
+}
+
+impl From<Client> for ClientDto {
+    fn from(value: Client) -> Self {
+        let DestructClient {
+            id,
+            name,
+            desc,
+            uris,
+            types,
+            scopes,
+        } = value.into_destruct();
+        Self { 
+            id: id.into(), 
+            name: name.into(), 
+            desc: desc.into(), 
+            uris: uris.map(|uris| 
+                uris.into_iter()
+                    .map(Into::into)
+                    .collect::<Vec<String>>()
+                ), 
+            types: types.into(), 
+            scopes: Vec::from(scopes).into_iter()
+                        .map(|obj: ScopedObject| ScopeDto::from(obj))
+                        .collect()
+        }
+    }
+}
+
+impl From<ScopedObject> for ScopeDto {
+    fn from(value: ScopedObject) -> Self {
+        let DestructScopedObject { 
+            method, 
+            description 
+        } = value.into_destruct();
+        Self { 
+            method: method.into(), 
+            description: description.into() 
+        }
+    }
+}
