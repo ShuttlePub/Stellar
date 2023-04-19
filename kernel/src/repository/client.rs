9d357852da8178342959e3716072a8a3e598d5c5
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-04-19 13:10:41 +0900

    🔥 - Removed `ClientScopeRegistry`.

--- kernel/src/repository/client.rs
+++ kernel/src/repository/client.rs
@@ -9,14 +9,3 @@ pub trait ClientRegistry: 'static + Sync + Send {
 
     async fn find_by_id(&self, id: &ClientId) -> Result<Option<Client>, KernelError>;
 }
-
-#[async_trait::async_trait]
-pub trait ClientScopeRegistry: 'static + Sync + Send {
-    async fn register(&self) -> Result<(), KernelError>;
-    async fn unregister(&self) -> Result<(), KernelError>;
-
-    async fn update(&self) -> Result<(), KernelError>;
-
-    async fn find_by_client_id(&self) -> Result<(), KernelError>;
-    async fn find_by_scope(&self) -> Result<(), KernelError>;
-}
