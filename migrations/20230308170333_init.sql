1d5feabb034d5f7d8812579fcd9d5aa5894df309
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-04-24 16:22:22 +0900

    🚧 - Interim save once the structure is boiled down XD.

--- migrations/20230308170333_init.sql
+++ migrations/20230308170333_init.sql
@@ -1,30 +1,99 @@
 CREATE TABLE users(
-  id      UUID         NOT NULL PRIMARY KEY,
-  address VARCHAR(128) NOT NULL UNIQUE,
+  id      UUID         NOT NULL,
+  address VARCHAR(128) NOT NULL,
   name    VARCHAR(128) NOT NULL,
   pass    VARCHAR(256) NOT NULL,
   created_at  TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp(),
   updated_at  TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp(),
-  verified_at TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp()
+  verified_at TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp(),
+
+  PRIMARY KEY (id, address)
 );
 
+CREATE TYPE TEP_AM
+  AS ENUM (
+    'client_secret_post',
+    'client_secret_basic',
+    'private_key_jwt',
+    'none'
+  );
+
+CREATE TYPE GRANT_TYPE
+  AS ENUM (
+    'authorization_code',
+    'implicit',
+    'password',
+    'client_credentials',
+    'refresh_token',
+    'urn:ietf:params:oauth:grant-type:jwt-bearer',
+    'urn:ietf:params:oauth:grant-type:saml2-bearer'
+  );
+
+CREATE TABLE __client(
+  id             UUID           NOT NULL PRIMARY KEY,
+  redirect_uris  VARCHAR(512)[] NOT NULL UNIQUE,
+  auth_method    TEP_AM         NOT NULL,
+  grant_type     GRANT_TYPE[]   NOT NULL,
+  response_types VARCHAR(512)[] NOT NULL,
+  client_name    VARCHAR(256)   NOT NULL,
+  client_uri     VARCHAR(512),
+  logo_uri       VARCHAR(512),
+  scopes         VARCHAR(128)[] NOT NULL,
+  req_scopes     VARCHAR(128)[],
+  contacts       VARCHAR(128)[] NOT NULL,
+  tos_uri        VARCHAR(512)   NOT NULL,
+  policy_uri     VARCHAR(512)   NOT NULL,
+  jwks           JSONB          NOT NULL,
+  software_id    VARCHAR(256),
+  software_ver   VARCHAR(128)
+);
+
+CREATE TABLE ___clients(
+  id          UUID         NOT NULL,
+  client_name VARCHAR(256) NOT NULL UNIQUE,
+  PRIMARY KEY(id, client_name)
+);
+
+CREATE TABLE client_metadata(
+  id             UUID           NOT NULL PRIMARY KEY,
+  client_id      UUID           NOT NULL,
+  client_uri     VARCHAR(512),
+  logo_uri       VARCHAR(512),
+  contact        VARCHAR(128)[] NOT NULL,
+  tos_uri        VARCHAR(512)   NOT NULL,
+  policy_uri     VARCHAR(512)   NOT NULL,
+  FOREIGN KEY (client_id) REFERENCES ___clients(id) ON DELETE CASCADE
+);
+
+CREATE TABLE client_cert(
+  -- TODO: Impl client authorization registry.
+);
+
+CREATE TABLE redirect_uris(
+  id        UUID NOT NULL,
+  uri       VARCHAR(512) NOT NULL,
+  client_id UUID NOT NULL,
+  PRIMARY KEY (id, uri),
+  FOREIGN KEY (client_id) REFERENCES ___clients(id) ON DELETE CASCADE
+);
+
+CREATE TABLE scopes(
+  id          UUID         NOT NULL PRIMARY KEY,
+  client_id   UUID         NOT NULL,
+  method      VARCHAR(64)  NOT NULL,
+  description VARCHAR(256) NOT NULL,
+
+  UNIQUE (method, client_id),
+  FOREIGN KEY (client_id) REFERENCES ___clients(id) ON DELETE CASCADE
+);
+
+-- Will deprecate.
 CREATE TABLE clients(
   id           UUID         NOT NULL PRIMARY KEY,
   name         VARCHAR(128) NOT NULL,
   description  TEXT,
   redirect_uri VARCHAR(256) NOT NULL,
   secret       VARCHAR(256)
-);
-
-CREATE TYPE access AS ENUM ('confidential', 'public');
-
-CREATE TABLE scopes(
-  id          UUID NOT NULL PRIMARY KEY,
-  client      UUID NOT NULL,
-  access      access      NOT NULL,
-  method      VARCHAR(64) NOT NULL,
-  description TEXT,
-  FOREIGN KEY (client) REFERENCES clients(id) ON DELETE CASCADE
 );
 
 CREATE TABLE token(
