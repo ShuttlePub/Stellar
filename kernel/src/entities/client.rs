59a04ab224a740826a4a09d26374553f7682cd75
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-04-27 13:24:08 +0900

    🗑️ - Marked deprecate Client.

--- kernel/src/entities/client.rs
+++ kernel/src/entities/client.rs
@@ -11,35 +11,36 @@ use super::{
     Scopes, UserId, 
 };
 
+#[deprecated]
 /// Client information based on RFC6749.
-/// 
+///
 /// See [RFC6749 Section 2](https://www.rfc-editor.org/rfc/rfc6749#section-2)
 #[derive(Debug, Clone, Hash, Deserialize, Serialize, Destructure)]
 pub struct Client {
     /// An identifier to identify the client.
-    /// 
+    ///
     /// Reference [RFC6749 Section 2.2](https://www.rfc-editor.org/rfc/rfc6749#section-2.2)
     id: ClientId,
     /// An name to display the client
     name: ClientName,
     /// An description to display the client
     desc: ClientDescription,
-    /// It is an absolute URI, 
-    /// that user will be redirected to 
+    /// It is an absolute URI,
+    /// that user will be redirected to
     /// when user finish the authorized server exchange.
-    /// 
+    ///
     /// Reference [RFC6749 Section 3.1.2](https://www.rfc-editor.org/rfc/rfc6749#section-3.1.2)
     uris: Option<Vec<RedirectUri>>,
     /// A client type, segregated by the client's ability to maintain confidentiality.
-    /// 
+    ///
     /// Reference [RFC6749 Section 2.1](https://www.rfc-editor.org/rfc/rfc6749#section-2.1)
     types: ClientTypes,
     /// Identifier of the user who owns this client.
-    /// 
+    ///
     /// The client **must** have a Stellar account.
     owner: UserId,
     /// A set of scopes allowed to users defined by the client.
-    /// 
+    ///
     /// Reference [RFC6749 Section 3.3](https://www.rfc-editor.org/rfc/rfc6749#section-3.3)
     scopes: Scopes,
 }
