6b9336eac335c6a84ee4cb8015443c5fc8cc26f8
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-04-28 11:32:37 +0900

    🔨 - Added field `expires_at` in `ClientSecret`.

--- kernel/src/entities/claims/client_secret.rs
+++ kernel/src/entities/claims/client_secret.rs
@@ -1,13 +1,34 @@
+use std::time::UNIX_EPOCH;
+use destructure::Destructure;
 use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
 
 use crate::KernelError;
 
-#[derive(Debug, Clone, Hash, PartialEq, Eq, Deserialize, Serialize)]
-pub struct ClientSecret(String);
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Deserialize, Serialize, Destructure)]
+pub struct ClientSecret {
+    secret: String,
+    expires_at: OffsetDateTime
+}
 
 impl ClientSecret {
     pub fn new(secret: impl Into<String>) -> Self {
-        Self(secret.into())
+        Self {
+            secret: secret.into(),
+            expires_at: OffsetDateTime::now_utc()
+        }
+    }
+
+    pub fn secret(&self) -> &str {
+        &self.secret
+    }
+
+    pub fn expires_at(&self) -> &OffsetDateTime {
+        &self.expires_at
+    }
+
+    pub fn expires_at_as_u64(&self) -> u64 {
+        (UNIX_EPOCH - self.expires_at).abs().whole_seconds() as u64
     }
 
     pub fn verify(&self, _secret: impl Into<String>) -> Result<(), KernelError> {
@@ -15,14 +36,20 @@ impl ClientSecret {
     }
 }
 
-impl From<ClientSecret> for String {
-    fn from(origin: ClientSecret) -> Self {
-        origin.0
-    }
-}
+#[cfg(test)]
+mod tests {
+    use rand::distributions::{Alphanumeric, Distribution};
+    use crate::entities::ClientSecret;
 
-impl AsRef<str> for ClientSecret {
-    fn as_ref(&self) -> &str {
-        &self.0
+    #[test]
+    fn test() -> anyhow::Result<()> {
+        let code = Alphanumeric.sample_iter(&mut rand::thread_rng())
+            .take(64)
+            .map(char::from)
+            .collect::<String>();
+        let secret = ClientSecret::new(code);
+        let exp = secret.expires_at_as_u64();
+        println!("{:?}", exp);
+        Ok(())
     }
 }
