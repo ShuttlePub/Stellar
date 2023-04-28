1a68b3789aac91fa045306feaf8008c97e632173
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-04-28 11:33:30 +0900

    🔨 - Added field `issued_at` in `ClientId`.

--- kernel/src/entities/claims/client_id.rs
+++ kernel/src/entities/claims/client_id.rs
@@ -1,31 +1,39 @@
 use std::fmt::Display;
+use destructure::Destructure;
 
 use serde::{Serialize, Deserialize};
+use time::OffsetDateTime;
 use uuid::Uuid;
 
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Deserialize, Serialize)]
-pub struct ClientId(Uuid);
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Deserialize, Serialize, Destructure)]
+pub struct ClientId {
+    id: Uuid,
+    issued_at: OffsetDateTime
+}
 
 impl ClientId {
-    pub fn new(id: impl Into<Uuid>) -> Self {
-        Self(id.into())
+    pub fn new(id: impl Into<Uuid>, iat: impl Into<OffsetDateTime>) -> Self {
+        Self { id: id.into(), issued_at: iat.into() }
     }
-}
 
-impl From<ClientId> for Uuid {
-    fn from(origin: ClientId) -> Self {
-        origin.0
+    pub fn new_at_now(id: impl Into<Uuid>) -> Self {
+        Self {
+            id: id.into(),
+            issued_at: OffsetDateTime::now_utc()
+        }
     }
-}
 
-impl AsRef<Uuid> for ClientId {
-    fn as_ref(&self) -> &Uuid {
-        &self.0
+    pub fn id(&self) -> &Uuid {
+        &self.id
+    }
+
+    pub fn issued_at(&self) -> &OffsetDateTime {
+        &self.issued_at
     }
 }
 
 impl Display for ClientId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+        write!(f, "id: {}, iat: {}", self.id, self.issued_at)
     }
 }
