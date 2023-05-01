cc12e2e2e2e7d54c133a2d7e1bab08b7421c115e
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2023-05-02 06:50:57 +0900

    🚨 - Fixed warning from `clippy`.

--- application/src/transfer/client.rs
+++ application/src/transfer/client.rs
@@ -104,7 +104,7 @@ impl From<Client> for ClientDto {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum TokenEndPointAuthMethodDto {
     ClientSecretPost,
     ClientSecretBasic,
