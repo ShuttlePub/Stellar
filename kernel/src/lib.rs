295338c8f2c5175e4edd71c59cfd48fedbfdf9b6
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-05-01 22:02:06 +0900

    🔨 - Defined static variable `BASE_URL`.

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -3,7 +3,17 @@ pub mod repository;
 pub mod transporter;
 mod error;
 
+use once_cell::sync::Lazy;
+use url::Url;
 pub use self::error::KernelError;
+
+pub(crate) static BASE_URL: Lazy<Url> = Lazy::new(|| {
+    dotenvy::var("BASE_URL").ok()
+        .map(|url| Url::parse(&url))
+        .transpose()
+        .expect("`BASE_URL` cannot parse url! This value require valid.")
+        .expect("`BASE_URL` does not set! This value require.")
+});
 
 #[cfg(feature = "url")]
 #[cfg(feature = "time")]
