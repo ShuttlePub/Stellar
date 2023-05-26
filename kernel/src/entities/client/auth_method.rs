aa3793b250616b3c48272bfb07385b5b4fe45f61
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-05-26 09:40:18 +0900

    🐛✏️ - Fixed serde handling.

--- kernel/src/entities/client/auth_method.rs
+++ kernel/src/entities/client/auth_method.rs
@@ -1,8 +1,8 @@
 use std::str::FromStr;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use crate::KernelError;
 
-#[derive(Debug, Clone, Hash, Deserialize, Serialize)]
+#[derive(Debug, Clone, Hash)]
 pub enum TokenEndPointAuthMethod {
     ClientSecretPost,
     ClientSecretBasic,
@@ -40,7 +40,25 @@ impl AsRef<str> for TokenEndPointAuthMethod {
             TokenEndPointAuthMethod::ClientSecretPost => "client_secret_post",
             TokenEndPointAuthMethod::ClientSecretBasic => "client_secret_basic",
             TokenEndPointAuthMethod::None => "none",
-            TokenEndPointAuthMethod::PrivateKeyJWT => "private_key_jwk"
+            TokenEndPointAuthMethod::PrivateKeyJWT => "private_key_jwt"
         }
+    }
+}
+
+impl<'de> Deserialize<'de> for TokenEndPointAuthMethod {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de>
+    {
+        let deserialize: &str = Deserialize::deserialize(deserializer)?;
+        Self::from_str(deserialize)
+            .map_err(serde::de::Error::custom)
+    }
+}
+
+impl Serialize for TokenEndPointAuthMethod {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_str(self.as_ref())
     }
 }
