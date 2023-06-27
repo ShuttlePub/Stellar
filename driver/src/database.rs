4157726b1cb17260c16653fc3f210a8dc27d5a14
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-06-27 10:14:23 +0900

    🔨 - Added a method to simplify the process of acquiring redis pools.

--- driver/src/database.rs
+++ driver/src/database.rs
@@ -16,4 +16,18 @@ pub use self::{
     mfa_code::*,
     session::*,
     ticket::*,
+    redis_pool::*,
 };
+
+pub(in crate::database) mod redis_pool {
+    use deadpool_redis::{Pool, Connection as RedisConnection};
+    use crate::error::DriverError;
+    
+    pub struct RedisPoolMng;
+
+    impl RedisPoolMng {
+        pub async fn acquire(pool: &Pool) -> Result<RedisConnection, DriverError> {
+            Ok(pool.get().await?)
+        }
+    }
+}
