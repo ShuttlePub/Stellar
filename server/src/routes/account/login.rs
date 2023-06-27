8d513284019d9660a354981286c060b4114a4765
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-06-27 10:25:49 +0900

    ♻️ - Refactored by Controller introduction.

--- server/src/routes/account/login.rs
+++ server/src/routes/account/login.rs
@@ -1,46 +1,84 @@
-use axum::{
-    headers::{HeaderMap, HeaderValue},
-    Form,
-    extract::State,
-    http::{header::SET_COOKIE, StatusCode},
-    response::IntoResponse
-};
-use application::{
-    services::VerifyAccountService,
-    transfer::account::VerifyAccountDto
-};
+use axum::{Form, extract::State, response::IntoResponse};
+use application::services::{VerifyAccountService, DependOnVerifyAccountService};
 use crate::{Handler, ServerError};
-use crate::extract::session::{Session, SESSION_TAG};
-use self::form::UserInput;
+use crate::controller::Controller;
+use crate::extract::session::Session;
+use self::form::*;
 
 pub async fn login(
     State(handler): State<Handler>,
     session: Session,
     Form(input): Form<UserInput>
 ) -> Result<impl IntoResponse, ServerError> {
-    let dto = VerifyAccountDto {
+    let req = Request {
         address: input.address,
         pass: input.pass,
-        ticket: input.code,
+        code: input.code,
         session: session.into(),
     };
 
-    let session = handler.verify(dto).await?;
+    let res = Controller::new(Transformer, Presenter)
+        .transform(req)
+        .handle(|req| async {
+            handler.verify_account_service()
+                .verify(req)
+                .await
+        }).await?;
 
-    let mut headers = HeaderMap::new();
-    headers.insert(SET_COOKIE, HeaderValue::from_str(format!("{}={}", SESSION_TAG, session.id).as_str())
-        .map_err(|e| ServerError::Axum(anyhow::Error::new(e)))?);
-
-    Ok((headers, StatusCode::OK))
+    Ok(res)
 }
 
 mod form {
+    use axum::{http::{HeaderMap, StatusCode}, headers::HeaderValue, http::header::SET_COOKIE};
     use serde::Deserialize;
+    use application::{transfer::account::VerifyAccountDto, ApplicationError, transfer::session::SessionDto};
+    use crate::{controller::{InputPort, OutputPort}, extract::session::SESSION_TAG, ServerError};
 
     #[derive(Deserialize)]
     pub struct UserInput {
         pub address: Option<String>,
         pub pass: Option<String>,
         pub code: Option<String>,
+    }
+
+    pub struct Request {
+        pub address: Option<String>,
+        pub pass: Option<String>,
+        pub code: Option<String>,
+        pub session: Option<String>
+    }
+
+    pub struct Transformer;
+
+    impl InputPort<Request> for Transformer {
+        type Dto = VerifyAccountDto;
+        fn emit(&self, input: Request) -> Self::Dto {
+            VerifyAccountDto {
+                address: input.address,
+                pass: input.pass,
+                ticket: input.code,
+                session: input.session,
+            }
+        }
+    }
+
+    pub struct Presenter;
+
+    impl OutputPort<Result<SessionDto, ApplicationError>> for Presenter {
+        type ViewModel = Result<(HeaderMap, StatusCode), ServerError>;
+        fn emit(&self, input: Result<SessionDto, ApplicationError>) -> Self::ViewModel {
+            match input {
+                Ok(session) => {
+                    let session = HeaderValue::from_str(format!("{}={}", SESSION_TAG, session.id).as_str())
+                        .map_err(|e| ServerError::Axum(anyhow::Error::new(e)))?;
+
+                    let mut headers = HeaderMap::new();
+                    headers.insert(SET_COOKIE, session);
+
+                    Ok((headers, StatusCode::OK))
+                },
+                Err(e) => { Err(e.into()) }
+            }
+        }
     }
 }
